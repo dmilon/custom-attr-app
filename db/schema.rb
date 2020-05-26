71789541aea341a40218c6638e5e89a92f747475
Sandra Vilder <sandra.vilder@gmail.com>
2020-05-26 15:54:38 +0200

    EventAttribute created

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_26_134614) do
+ActiveRecord::Schema.define(version: 2020_05_26_135108) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "event_attributes", force: :cascade do |t|
+    t.string "name"
+    t.boolean "required", default: false
+    t.string "data_type"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "events", force: :cascade do |t|
     t.string "name"
